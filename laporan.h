6afff0a6d1c2d03bbe7bf84c1c639318e4b68e07
LIAFARÉON <liafareon@gmail.com>
2026-05-18 23:16:01 +0700

    Update laporan.h

--- laporan.h
+++ laporan.h
@@ -1,1 +1,28 @@
+#ifndef LAPORAN_H
+#define LAPORAN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ================================
+// STRUKTUR MENU MAKANAN
+// ================================
+typedef struct Menu {
+    char nama[50];
+    float harga;
+    int stok;
+    int terjual;
+    struct Menu* next;
+} Menu;
+
+// ================================
+// FUNGSI LAPORAN
+// ================================
+void insertionSort(Menu arr[], int n);
+void tampilkanLaporan(Menu* head);
+float hitungTotalPendapatan(Menu arr[], int n);
+int hitungJumlahMenu(Menu* head);
+void copyLinkedListKeArray(Menu* head, Menu arr[]);
+
+#endif
