354e15ddab1e9cc060e61abf4bb3a712e9324d39
LIAFARÉON <liafareon@gmail.com>
2026-05-18 23:36:25 +0700

    Update laporan.h

--- laporan.h
+++ laporan.h
@@ -5,24 +5,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-// ================================
-// STRUKTUR MENU MAKANAN
-// ================================
+#define TABLE_SIZE 10
+
+// ======================================
+// STRUCT MENU
+// ======================================
 typedef struct Menu {
+
     char nama[50];
     float harga;
     int stok;
     int terjual;
+
     struct Menu* next;
+
 } Menu;
 
-// ================================
-// FUNGSI LAPORAN
-// ================================
-void insertionSort(Menu arr[], int n);
-void tampilkanLaporan(Menu* head);
-float hitungTotalPendapatan(Menu arr[], int n);
+// ======================================
+// FUNCTION PROTOTYPE
+// ======================================
 int hitungJumlahMenu(Menu* head);
-void copyLinkedListKeArray(Menu* head, Menu arr[]);
+
+void copyLinkedListKeArray(
+    Menu* head,
+    Menu arr[]
+);
+
+void insertionSort(
+    Menu arr[],
+    int n
+);
+
+float hitungTotalPendapatan(
+    Menu arr[],
+    int n
+);
+
+void tampilkanLaporan(
+    Menu* head
+);
+
+Menu* buatMenu(
+    char nama[],
+    float harga,
+    int stok,
+    int terjual
+);
+
+void tambahMenu(
+    Menu** head,
+    char nama[],
+    float harga,
+    int stok,
+    int terjual
+);
 
 #endif
